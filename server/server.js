8e89c75260b1cc0415ad94f5b149b9b72e319d27
ThayaCheva <thayajjj@gmail.com>
2024-07-18 14:23:09 +1000

    added bodyparser

--- server/server.js
+++ server/server.js
@@ -2,30 +2,28 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors'); // Import CORS middleware
 const taskRoutes = require('./routes/task-routes');
 const userRoutes = require('./routes/user-routes');
-
+// express app
+const bodyParser = require('body-parser');
 const app = express();
 
-// Enable CORS
-app.use(cors());
-
-// Other middleware and route definitions
-app.use(express.json());
+// Express 4.0
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
+
+// Middleware
+app.use(express.json());
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 
-// Routes
+//routes
 app.use('/api/tasks', taskRoutes);
 app.use('/api/user', userRoutes);
-
-// Connect to MongoDB
+// connect to db
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
@@ -34,5 +32,5 @@ mongoose
     });
   })
   .catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
+    console.log(error);
   });
